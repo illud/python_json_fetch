b7e17865f9e0fd6936f62eab97f7714cce190458
illud <42035117+illud@users.noreply.github.com>
2018-10-09 15:46:43 -0500

    Added save to file in json format

--- python_json_fetch.py
+++ python_json_fetch.py
@@ -1,9 +1,13 @@
+#import library's
 import urllib, json
-
+#create variable with url to fetch
 url = "https://facebook.github.io/react-native/movies.json"
-
+#get the response
 response = urllib.urlopen(url)
-
+#load response and save to data
 data = json.loads(response.read())
-
+#print json data
 print data
+#create a json file and write data
+with open('data.json', 'w') as outfile:
+    json.dump(data, outfile, indent=4)
